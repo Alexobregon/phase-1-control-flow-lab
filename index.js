22f1fdcd98abfecc6f97359257842545cde0a8ab
Alexander Obregon <alexander.obregon47@gmail.com>
2021-12-13 16:20:07 -0800

    commit tree bfc780127a591cdfcbd9c471c27bf642db1909b8

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber) {
+  let result;
+  if (someNumber <= 400) {
+    result = someNumber = "This one is on me!";
+    return result;  
+    } else if (someNumber > 2000  && someNumber < 2500) {
+      result = someNumber = 'I will gladly take your thirty bucks.';
+      return result;
+    } else if (someNumber > 2500 ) {
+      result = someNumber = 'No can do.';
+      return result;
+     }
+  
+  scuberGreetingForFeet(2600);
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(City){
+
+
+return ((City === "NYC") ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+
+    switch (tip) {
+      case 'generous':
+        return "Thank you so much.";
+        break;
+      case 'not as generous':
+          return "Thank you.";
+          break;
+      case 'thanks for everything':
+          return "Bye.";
+          break;
+
+
+  }
 }
